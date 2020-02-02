42da079f952f969d3263e0e95d72c9cda2ffc26c
Irma Santos <irma.santos@gmail.com>
2020-02-02 22:31:59 +0000

    criação do projecto inicial

---
+++ restaurantfood-api/src/main/java/com/irmamsantos/restaurantfood/RestaurantfoodApiApplication.java
@@ -0,0 +1,13 @@
+package com.irmamsantos.restaurantfood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantfoodApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantfoodApiApplication.class, args);
+	}
+
+}
---
+++ restaurantfood-api/src/test/java/com/irmamsantos/restaurantfood/RestaurantfoodApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.irmamsantos.restaurantfood;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantfoodApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
