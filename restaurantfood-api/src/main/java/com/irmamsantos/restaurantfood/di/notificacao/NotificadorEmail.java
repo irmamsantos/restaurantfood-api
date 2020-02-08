a3f13dc4ebe0e285726e0f667f0a26796311b5f6
Irma Santos <irma.santos@gmail.com>
2020-02-08 23:40:06 +0000

    2.25. Criando e acessando propriedades customizadas com @Value

--- restaurantfood-api/src/main/java/com/irmamsantos/restaurantfood/di/notificacao/NotificadorEmail.java
+++ restaurantfood-api/src/main/java/com/irmamsantos/restaurantfood/di/notificacao/NotificadorEmail.java
@@ -1,6 +1,7 @@
 package com.irmamsantos.restaurantfood.di.notificacao;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
@@ -11,11 +12,20 @@ import com.irmamsantos.restaurantfood.di.modelo.Cliente;
 @Component
 public class NotificadorEmail implements Notificador  {
 	
+	@Value("${notificador.email.host-servidor}")
+	private String host;
+	
+	@Value("${notificador.email.porta-servidor}")
+	private String porta;
+	
 	public NotificadorEmail() {
 		System.out.println("Notificador mail REAL");
 	}
 	
 	public void notificar(Cliente cliente, String mensagem) {
+		
+		System.out.println("Host: " + host);
+		System.out.println("Porta: " + porta);
 		
 		System.out.printf("Notificando %s através do e-mail %s: %s\n", 
 				cliente.getNome(), cliente.getEmail(), mensagem);
