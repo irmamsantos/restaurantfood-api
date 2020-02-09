1b4feb4ec2b52a810fe717bbf843bbf9c4777fda
Irma Santos <irma.santos@gmail.com>
2020-02-09 18:17:26 +0000

    3.8. Consultando objetos do banco de dados

---
+++ restaurantfood-api/src/main/java/com/irmamsantos/restaurantfood/jpa/CadastroCozinha.java
@@ -0,0 +1,24 @@
+package com.irmamsantos.restaurantfood.jpa;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+
+import org.springframework.stereotype.Component;
+
+import com.irmamsantos.restaurantfood.domain.model.Cozinha;
+
+@Component
+public class CadastroCozinha {
+	
+	@PersistenceContext
+	private EntityManager manager;
+
+	public List<Cozinha> listar() {
+		TypedQuery<Cozinha> typedQuery = manager.createQuery("from Cozinha", Cozinha.class);
+		List<Cozinha> cozinhas = typedQuery.getResultList();
+		return cozinhas;
+	}
+}
---
+++ restaurantfood-api/src/main/java/com/irmamsantos/restaurantfood/jpa/ConsultaCozinhaMain.java
@@ -0,0 +1,28 @@
+package com.irmamsantos.restaurantfood.jpa;
+
+import java.util.List;
+
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ApplicationContext;
+
+import com.irmamsantos.restaurantfood.RestaurantfoodApiApplication;
+import com.irmamsantos.restaurantfood.domain.model.Cozinha;
+
+public class ConsultaCozinhaMain {
+	
+	public static void main(String[] args) {
+		ApplicationContext applicationContext = new SpringApplicationBuilder(RestaurantfoodApiApplication.class)
+				.web(WebApplicationType.NONE)
+				.run(args);
+		
+		CadastroCozinha cadastroCozinha = applicationContext.getBean(CadastroCozinha.class);
+		
+		List<Cozinha> cozinhas = cadastroCozinha.listar();
+
+		for (Cozinha cozinha : cozinhas) {
+			System.out.println("Cozinha: " + cozinha.getNome());
+		}
+	}
+
+}
