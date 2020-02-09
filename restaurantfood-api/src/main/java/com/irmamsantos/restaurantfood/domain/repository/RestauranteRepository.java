ab11b22ec38ce9b5b9bd4e37c0c604787551f722
Irma Santos <irma.santos@gmail.com>
2020-02-09 23:52:51 +0000

    3.16. Lombok e repositório de restaurantes

---
+++ restaurantfood-api/src/main/java/com/irmamsantos/restaurantfood/domain/repository/RestauranteRepository.java
@@ -0,0 +1,13 @@
+package com.irmamsantos.restaurantfood.domain.repository;
+
+import java.util.List;
+
+import com.irmamsantos.restaurantfood.domain.model.Restaurante;
+
+public interface RestauranteRepository {
+	
+	List<Restaurante> todas();
+	Restaurante porId(Long id);
+	Restaurante adicionar(Restaurante restaurante);
+	void remover(Restaurante restaurante);
+}
---
+++ restaurantfood-api/src/main/java/com/irmamsantos/restaurantfood/infrastructure/repository/RestauranteRepositoryImpl.java
@@ -0,0 +1,42 @@
+package com.irmamsantos.restaurantfood.infrastructure.repository;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.irmamsantos.restaurantfood.domain.model.Restaurante;
+import com.irmamsantos.restaurantfood.domain.repository.RestauranteRepository;
+
+@Component
+public class RestauranteRepositoryImpl implements RestauranteRepository {
+
+	@PersistenceContext
+	private EntityManager manager;
+
+	public List<Restaurante> todas() {
+		TypedQuery<Restaurante> typedQuery = manager.createQuery("from Restaurante", Restaurante.class);
+		List<Restaurante> restaurantes = typedQuery.getResultList();
+		return restaurantes;
+	}
+
+	public Restaurante porId(Long id) {
+		return manager.find(Restaurante.class, id);
+	}
+	
+	@Transactional
+	public Restaurante adicionar(Restaurante restaurante) {
+		return manager.merge(restaurante);
+	}
+	
+	@Transactional
+	public void remover(Restaurante restaurante) {
+		Restaurante restauranteApagar = porId(restaurante.getId());
+		manager.remove(restauranteApagar);
+	}
+
+}
