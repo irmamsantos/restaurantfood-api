65f5b68c0ec2f78ec2b4c9b77b0f47da1020cddd
Irma Santos <irma.santos@gmail.com>
2020-02-10 00:22:08 +0000

    3.17. Mapeando relacionamento com @ManyToOne

---
+++ restaurantfood-api/src/main/java/com/irmamsantos/restaurantfood/jpa/ConsultaRestauranteMain.java
@@ -0,0 +1,29 @@
+package com.irmamsantos.restaurantfood.jpa;
+
+import java.util.List;
+
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ApplicationContext;
+
+import com.irmamsantos.restaurantfood.RestaurantfoodApiApplication;
+import com.irmamsantos.restaurantfood.domain.model.Restaurante;
+import com.irmamsantos.restaurantfood.domain.repository.RestauranteRepository;
+
+public class ConsultaRestauranteMain {
+	
+	public static void main(String[] args) {
+		ApplicationContext applicationContext = new SpringApplicationBuilder(RestaurantfoodApiApplication.class)
+				.web(WebApplicationType.NONE)
+				.run(args);
+		
+		RestauranteRepository restauranteRepository = applicationContext.getBean(RestauranteRepository.class);
+		
+		List<Restaurante> restaurantes = restauranteRepository.todas();
+
+		for (Restaurante restaurante : restaurantes) {
+			System.out.printf("%s - %f - %s\n", restaurante.getNome(), restaurante.getTaxaFrete(), restaurante.getCozinha().getNome());
+		}
+	}
+
+}
