4aa74353c932fba6601d0be033f112c4014e4f06
Irma Santos <irma.santos@gmail.com>
2020-02-15 08:53:05 +0000

    4.11. collection resource de estados

---
+++ restaurantfood-api/src/main/java/com/irmamsantos/restaurantfood/api/controller/EstadoController.java
@@ -0,0 +1,25 @@
+package com.irmamsantos.restaurantfood.api.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.irmamsantos.restaurantfood.domain.model.Estado;
+import com.irmamsantos.restaurantfood.domain.repository.EstadoRepository;
+
+@RestController
+@RequestMapping("/estados")
+public class EstadoController {
+	
+	@Autowired
+	private EstadoRepository estadoRepository; 
+	
+	@GetMapping
+	public List<Estado> listar() {
+		return estadoRepository.todas();
+	}
+
+}
