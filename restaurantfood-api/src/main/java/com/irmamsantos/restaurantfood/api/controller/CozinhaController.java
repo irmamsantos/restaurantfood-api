6d7007655bda39ee577d820a5295daed827c2efe
Irma Santos <irma.santos@gmail.com>
2020-02-15 19:17:33 +0000

    4.21. Corrigindo o Status HTTP para resource inexistente

--- restaurantfood-api/src/main/java/com/irmamsantos/restaurantfood/api/controller/CozinhaController.java
+++ restaurantfood-api/src/main/java/com/irmamsantos/restaurantfood/api/controller/CozinhaController.java
@@ -36,16 +36,13 @@ public class CozinhaController {
 	@GetMapping("/{cozinhaId}")
 	public ResponseEntity<Cozinha> buscar(@PathVariable("cozinhaId") Long id) {
 		Cozinha cozinha = cozinhaRepository.porId(id);
-		//return ResponseEntity.status(HttpStatus.OK).body(cozinha);
-		//return ResponseEntity.ok(cozinha);
 		
-		HttpHeaders headers = new HttpHeaders();
-		headers.add(HttpHeaders.LOCATION, "http://localhost:8080/cozinhas");
-		
-		return ResponseEntity
-				.status(HttpStatus.FOUND)
-				.headers(headers)
-				.build();
+		if (cozinha != null) {
+			//return ResponseEntity.status(HttpStatus.OK).body(cozinha);
+			return ResponseEntity.ok(cozinha);
+		}
+		//return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+		return ResponseEntity.notFound().build();
 	}
 	
 }
