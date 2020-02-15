2c1af770f1d41826f4e586e7fac7a25d0af89e3c
Irma Santos <irma.santos@gmail.com>
2020-02-15 19:07:24 +0000

    4.20. Manipulando a resposta HTTP com ResponseEntity

--- restaurantfood-api/src/main/java/com/irmamsantos/restaurantfood/api/controller/CozinhaController.java
+++ restaurantfood-api/src/main/java/com/irmamsantos/restaurantfood/api/controller/CozinhaController.java
@@ -3,7 +3,10 @@ package com.irmamsantos.restaurantfood.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -31,8 +34,18 @@ public class CozinhaController {
 	}
 	
 	@GetMapping("/{cozinhaId}")
-	public Cozinha buscar(@PathVariable("cozinhaId") Long id) {
-		return cozinhaRepository.porId(id);
+	public ResponseEntity<Cozinha> buscar(@PathVariable("cozinhaId") Long id) {
+		Cozinha cozinha = cozinhaRepository.porId(id);
+		//return ResponseEntity.status(HttpStatus.OK).body(cozinha);
+		//return ResponseEntity.ok(cozinha);
+		
+		HttpHeaders headers = new HttpHeaders();
+		headers.add(HttpHeaders.LOCATION, "http://localhost:8080/cozinhas");
+		
+		return ResponseEntity
+				.status(HttpStatus.FOUND)
+				.headers(headers)
+				.build();
 	}
 	
 }
