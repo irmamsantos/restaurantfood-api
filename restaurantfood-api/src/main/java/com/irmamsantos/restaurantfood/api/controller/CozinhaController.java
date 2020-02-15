d797c1027ef0471bb3f993e7650291de56ce27fc
Irma Santos <irma.santos@gmail.com>
2020-02-15 08:43:31 +0000

    4.10. Modelando e requisitando um Collection Resource com GET

---
+++ restaurantfood-api/src/main/java/com/irmamsantos/restaurantfood/api/controller/CozinhaController.java
@@ -0,0 +1,25 @@
+package com.irmamsantos.restaurantfood.api.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.irmamsantos.restaurantfood.domain.model.Cozinha;
+import com.irmamsantos.restaurantfood.domain.repository.CozinhaRepository;
+
+@RestController
+@RequestMapping("/cozinhas")
+public class CozinhaController {
+	
+	@Autowired
+	private CozinhaRepository cozinhaRepository; 
+	
+	@GetMapping
+	public List<Cozinha> listar() {
+		return cozinhaRepository.todas();
+	}
+
+}
