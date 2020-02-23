7aa74778b6c4b52fd422cfec14e7985a139aad8d
Irma Santos <irma.santos@gmail.com>
2020-02-23 23:45:51 +0000

    7.12. Desafio: Criando migrações e mapeando as entidades Pedido e ItemPedido

---
+++ restaurantfood-api/src/main/java/com/irmamsantos/restaurantfood/domain/model/Pedido.java
@@ -0,0 +1,83 @@
+package com.irmamsantos.restaurantfood.domain.model;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+public class Pedido {
+	
+	@EqualsAndHashCode.Include
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(nullable=false)
+	private BigDecimal subTotal;
+	
+	@Column(nullable=false)
+	private BigDecimal taxaFrete;
+	
+	@Column(nullable=false)
+	private BigDecimal valorTotal;
+	
+	@JsonIgnore
+	@CreationTimestamp
+	@Column(nullable = false, columnDefinition="datetime")
+	private LocalDateTime dataCriacao;
+	
+	@JsonIgnore
+	@Column(columnDefinition="datetime")
+	private LocalDateTime dataConfirmacao;
+	
+	@JsonIgnore
+	@Column(columnDefinition="datetime")
+	private LocalDateTime dataCancelamento;
+
+	@JsonIgnore
+	@Column(columnDefinition="datetime")
+	private LocalDateTime dataEntrega;
+	
+	@ManyToOne
+	@JoinColumn(name="forma_pagamento_id", nullable = false)
+	private FormaPagamento formaPagamento;
+	
+	@OneToMany(mappedBy = "pedido")
+	private List<ItemPedido> itens = new ArrayList<>();
+	
+	@Enumerated(EnumType.STRING)
+	private StatusPedido status;
+	
+	@Embedded
+	private Endereco enderecoEntrega;
+	
+	@ManyToOne
+	@JoinColumn(name="ususario_cliente_id")
+	private Usuario cliente;
+	
+	@ManyToOne
+	@JoinColumn(name="restaurante_id", nullable = false)
+	private Restaurante restaurante;
+}
