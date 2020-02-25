27cf1e08ad1cda7a32e3f4e56f109cab3e7f31dc
Irma Santos <irma.santos@gmail.com>
2020-02-25 15:59:23 +0000

    8.24. Lançando exception de desserialização na atualização parcial (PATCH)

--- restaurantfood-api/src/main/java/com/irmamsantos/restaurantfood/api/controller/RestauranteController.java
+++ restaurantfood-api/src/main/java/com/irmamsantos/restaurantfood/api/controller/RestauranteController.java
@@ -4,10 +4,15 @@ import java.lang.reflect.Field;
 import java.util.List;
 import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.http.server.ServletServerHttpRequest;
 import org.springframework.util.ReflectionUtils;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,6 +25,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.irmamsantos.restaurantfood.domain.exception.CozinhaNaoEncontradaException;
 import com.irmamsantos.restaurantfood.domain.exception.EntidadeEmUsoException;
@@ -116,12 +122,12 @@ public class RestauranteController {
 	
 	@PatchMapping("/{restauranteId}")
 	public Restaurante actualizarParcial(@PathVariable Long restauranteId, 
-			@RequestBody Map<String, Object> campos) 
+			@RequestBody Map<String, Object> campos, HttpServletRequest request) 
 					throws RestauranteNaoEncontradoException, NegocioException {
 		
 		Restaurante restauranteActual = restauranteService.buscarOuFalhar(restauranteId);
 		
-		merge(campos, restauranteActual);
+		merge(campos, restauranteActual, request);
 		
 		return actualizar(restauranteId, restauranteActual);
 /*		
@@ -155,21 +161,31 @@ public class RestauranteController {
 */		
 	}
 	
-	private void merge(Map<String, Object> dadosOrigem, Restaurante restauranteDestino) {
-		
-		ObjectMapper objectMapper =  new ObjectMapper();
-		Restaurante restauranteOrigem = objectMapper.convertValue(dadosOrigem, Restaurante.class);
-		
-		dadosOrigem.forEach((nomePropriedade, valorPropriedade) -> {
-			
-			Field field = ReflectionUtils.findField(Restaurante.class, nomePropriedade);
-			field.setAccessible(true);
-			
-			Object novoValor = ReflectionUtils.getField(field, restauranteOrigem);
-			
-			System.out.println(nomePropriedade + " = " + valorPropriedade + " = " + novoValor);
-			
-			ReflectionUtils.setField(field, restauranteDestino, novoValor);
-		});
+	private void merge(Map<String, Object> dadosOrigem, Restaurante restauranteDestino, HttpServletRequest request) {
+
+		ServletServerHttpRequest httpRequest = new ServletServerHttpRequest(request);
+
+		try {
+			ObjectMapper objectMapper = new ObjectMapper();
+			objectMapper.configure(DeserializationFeature.FAIL_ON_IGNORED_PROPERTIES, true);
+			objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, true);
+
+			Restaurante restauranteOrigem = objectMapper.convertValue(dadosOrigem, Restaurante.class);
+
+			dadosOrigem.forEach((nomePropriedade, valorPropriedade) -> {
+
+				Field field = ReflectionUtils.findField(Restaurante.class, nomePropriedade);
+				field.setAccessible(true);
+
+				Object novoValor = ReflectionUtils.getField(field, restauranteOrigem);
+
+				System.out.println(nomePropriedade + " = " + valorPropriedade + " = " + novoValor);
+
+				ReflectionUtils.setField(field, restauranteDestino, novoValor);
+			});
+		} catch (IllegalArgumentException ex) {
+			Throwable rootCause = ExceptionUtils.getRootCause(ex);
+			throw new HttpMessageNotReadableException(ex.getMessage(), rootCause, httpRequest);
+		}
 	}
 }
