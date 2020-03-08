d49ff0885c465cb4c4cd12c7e7802d6167722acb
Irma Santos <irma.santos@gmail.com>
2020-03-08 20:16:10 +0000

    10.9. Validando o corpo da resposta HTTP

--- restaurantfood-api/src/test/java/com/irmamsantos/restaurantfood/CadastroCozinhaIT.java
+++ restaurantfood-api/src/test/java/com/irmamsantos/restaurantfood/CadastroCozinhaIT.java
@@ -1,6 +1,9 @@
 package com.irmamsantos.restaurantfood;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.hasItems;
+
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -34,6 +37,22 @@ public class CadastroCozinhaIT {
 			.get()
 		.then()
 			.statusCode(HttpStatus.OK.value());
+	}
+	
+	@Test
+	public void deveConter4Cozinhas_QuandoConsultarCozinhas() {
+		
+		RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+		
+		given()
+			.basePath("/cozinhas")
+			.port(portParam)
+			.accept(ContentType.JSON)
+		.when()
+			.get()
+		.then()
+			.body("", hasSize(4))
+			.body("nome", hasItems("Indiana", "Tailandesa"));
 	}
 	
 /* Exemplos de Testes de Integração
