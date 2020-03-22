5324bb822155b1c5632ccc199705e3e196f72408
Irma Santos <irma.santos@gmail.com>
2020-03-22 10:57:43 +0000

    11.14. Adicionando e usando o ModelMapper

--- restaurantfood-api/src/main/java/com/irmamsantos/restaurantfood/api/assembler/RestauranteInputDTODisassembler.java
+++ restaurantfood-api/src/main/java/com/irmamsantos/restaurantfood/api/assembler/RestauranteInputDTODisassembler.java
@@ -1,36 +1,24 @@
 package com.irmamsantos.restaurantfood.api.assembler;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import com.irmamsantos.restaurantfood.api.model.dto.input.CozinhaIdInputDTO;
 import com.irmamsantos.restaurantfood.api.model.dto.input.RestauranteInputDTO;
-import com.irmamsantos.restaurantfood.domain.model.Cozinha;
 import com.irmamsantos.restaurantfood.domain.model.Restaurante;
 
 @Component
 public class RestauranteInputDTODisassembler {
+	
+	@Autowired
+	private ModelMapper modelMapper;
 
 	public Restaurante toDomainObject(RestauranteInputDTO restauranteInput) {
-		Restaurante restaurante = new Restaurante();
-		restaurante.setNome(restauranteInput.getNome());
-		restaurante.setTaxaFrete(restauranteInput.getTaxaFrete());
 		
-		Cozinha cozinha = new Cozinha();
-		cozinha.setId(restauranteInput.getCozinha().getId());
-		restaurante.setCozinha(cozinha);;
-		
-		return restaurante;
+		return modelMapper.map(restauranteInput, Restaurante.class);
 	}
 	
 	public RestauranteInputDTO domainObjectToDTO(Restaurante restaurante) {
-		RestauranteInputDTO restauranteInput = new RestauranteInputDTO();
-		
-		restauranteInput.setNome(restaurante.getNome());
-		restauranteInput.setTaxaFrete(restaurante.getTaxaFrete());
-		CozinhaIdInputDTO cozinhaIdInput = new CozinhaIdInputDTO();
-		cozinhaIdInput.setId(restaurante.getCozinha().getId());
-		restauranteInput.setCozinha(cozinhaIdInput);
-		
-		return restauranteInput;
+		return modelMapper.map(restaurante, RestauranteInputDTO.class);
 	}
 }
