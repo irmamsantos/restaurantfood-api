acc07100d84e7ae73a5f2dbbdb5bc718f7657ea8
Irma Santos <irma.santos@gmail.com>
2020-04-13 12:38:45 +0100

    13.15 Desafio: adicionando os filtros na consulta de vendas diárias

--- restaurantfood-api/src/main/java/com/irmamsantos/restaurantfood/infrastructure/service/VendaQueryServiceImpl.java
+++ restaurantfood-api/src/main/java/com/irmamsantos/restaurantfood/infrastructure/service/VendaQueryServiceImpl.java
@@ -1,5 +1,6 @@
 package com.irmamsantos.restaurantfood.infrastructure.service;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -7,14 +8,17 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.criteria.CompoundSelection;
 import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaBuilder.In;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Expression;
+import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
 
 import org.springframework.stereotype.Repository;
 
 import com.irmamsantos.restaurantfood.domain.filter.VendaDiariaFilter;
 import com.irmamsantos.restaurantfood.domain.model.Pedido;
+import com.irmamsantos.restaurantfood.domain.model.StatusPedido;
 import com.irmamsantos.restaurantfood.domain.model.dto.VendaDiaria;
 import com.irmamsantos.restaurantfood.domain.service.VendaQueryService;
 
@@ -38,6 +42,27 @@ public class VendaQueryServiceImpl implements VendaQueryService {
 		CriteriaQuery<VendaDiaria> query = builder.createQuery(VendaDiaria.class);
 		Root<Pedido> root = query.from(Pedido.class);
 		
+		List<Predicate> predicates = new ArrayList<Predicate>();
+		
+		//adicionar predicates no arrayList
+		if (filtro.getRestauranteId() != null) {
+			predicates.add(builder.equal(root.get("restaurante"), filtro.getRestauranteId()));
+		}
+		
+		if (filtro.getDataCriacaoInicio() != null) {
+			predicates.add(builder.greaterThanOrEqualTo(root.get("dataCriacao")
+					, filtro.getDataCriacaoInicio()));
+		}
+		
+		if (filtro.getDataCriacaoFim() != null) {
+			predicates.add(builder.lessThanOrEqualTo(root.get("dataCriacao")
+					, filtro.getDataCriacaoFim()));
+		}
+		
+		//status in ENTREGUE e CONFIRMADO
+		predicates.add(root.get("status").in(
+				StatusPedido.CONFIRMADO, StatusPedido.ENTREGUE));
+		
 		//date do sql é uma função da bd
 		Expression<Date> functionDateDataCricao = builder.function(
 				"date", Date.class, root.get("dataCriacao"));
@@ -48,6 +73,7 @@ public class VendaQueryServiceImpl implements VendaQueryService {
 				builder.sum(root.get("valorTotal")));
 		
 		query.select(selection);
+		query.where(predicates.toArray(new Predicate[0]));
 		query.groupBy(functionDateDataCricao);
 		
 		return manager.createQuery(query).getResultList();
