9123fffc49cc947505b6bdd1888cfb83a7ca3869
Irma Santos <irma.santos@gmail.com>
2020-05-02 23:06:37 +0100

    14.04 Desafio: Validando o content type do arquivo

--- restaurantfood-api/src/main/java/com/irmamsantos/restaurantfood/api/model/dto/input/FotoProdutoInputDTO.java
+++ restaurantfood-api/src/main/java/com/irmamsantos/restaurantfood/api/model/dto/input/FotoProdutoInputDTO.java
@@ -3,8 +3,10 @@ package com.irmamsantos.restaurantfood.api.model.dto.input;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
+import org.springframework.http.MediaType;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.irmamsantos.restaurantfood.core.validation.FileContentType;
 import com.irmamsantos.restaurantfood.core.validation.FileSize;
 
 import lombok.Getter;
@@ -16,6 +18,7 @@ public class FotoProdutoInputDTO {
 	
 	@NotNull
 	@FileSize(max = "50KB")
+	@FileContentType(allowed = { MediaType.IMAGE_JPEG_VALUE, MediaType.IMAGE_PNG_VALUE })
 	private MultipartFile arquivo;
 	
 	@NotBlank
