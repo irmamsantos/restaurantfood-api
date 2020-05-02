4ad753c83afc24b7b18d4d43f9410563e67f2afd
Irma Santos <irma.santos@gmail.com>
2020-05-02 22:48:11 +0100

    14.03 Validando o tamanho máximo do arquivo

--- restaurantfood-api/src/main/java/com/irmamsantos/restaurantfood/api/controller/RestauranteProdutoFotoController.java
+++ restaurantfood-api/src/main/java/com/irmamsantos/restaurantfood/api/controller/RestauranteProdutoFotoController.java
@@ -3,13 +3,13 @@ package com.irmamsantos.restaurantfood.api.controller;
 import java.nio.file.Path;
 import java.util.UUID;
 
+import javax.validation.Valid;
+
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
 
 import com.irmamsantos.restaurantfood.api.model.dto.input.FotoProdutoInputDTO;
 
@@ -20,7 +20,7 @@ public class RestauranteProdutoFotoController {
 	@PutMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
 	public void atualizarFoto(@PathVariable Long restauranteId,
 			@PathVariable Long produtoId, /*@RequestParam MultipartFile arquivo*/
-			FotoProdutoInputDTO fotoProdutoInput) {
+			@Valid FotoProdutoInputDTO fotoProdutoInput) {
 		String nomeArquivo = UUID.randomUUID().toString() 
 				+ "_" + fotoProdutoInput.getArquivo().getOriginalFilename();
 		
--- restaurantfood-api/src/main/java/com/irmamsantos/restaurantfood/api/model/dto/input/FotoProdutoInputDTO.java
+++ restaurantfood-api/src/main/java/com/irmamsantos/restaurantfood/api/model/dto/input/FotoProdutoInputDTO.java
@@ -1,6 +1,11 @@
 package com.irmamsantos.restaurantfood.api.model.dto.input;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
 import org.springframework.web.multipart.MultipartFile;
+
+import com.irmamsantos.restaurantfood.core.validation.FileSize;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -9,6 +14,10 @@ import lombok.Setter;
 @Setter
 public class FotoProdutoInputDTO {
 	
+	@NotNull
+	@FileSize(max = "50KB")
 	private MultipartFile arquivo;
+	
+	@NotBlank
 	private String descricao;
 }
