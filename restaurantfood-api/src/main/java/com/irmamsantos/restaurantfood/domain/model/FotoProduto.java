279b7f3a6b934631c3011b62372e164011c13d57
Irma Santos <irma.santos@gmail.com>
2020-05-02 23:32:40 +0100

    14.05 Mapeando entidade FotoProduto e relacionamento um-para-um

---
+++ restaurantfood-api/src/main/java/com/irmamsantos/restaurantfood/domain/model/FotoProduto.java
@@ -0,0 +1,35 @@
+package com.irmamsantos.restaurantfood.domain.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.MapsId;
+import javax.persistence.OneToOne;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+public class FotoProduto {
+	
+	@EqualsAndHashCode.Include
+	@Id
+	@Column(name = "produto_id")
+	private Long id;
+	
+	@OneToOne(fetch = FetchType.LAZY)
+	/*
+	 indica que estás usar a foreign key com chave
+	 */
+	@MapsId
+	private Produto produto;
+	
+	private String nomeArquivo;
+	private String descricao;
+	private String contentType;
+	private Long tamanho;
+	
+}
