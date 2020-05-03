6ebd56daf8eb95b75b7ac1932fcfc4866cee96c7
Irma Santos <irma.santos@gmail.com>
2020-05-03 23:03:55 +0100

    14.11 Desafio: Implementando recuperação de foto no serviço de armazenagem

--- restaurantfood-api/src/main/java/com/irmamsantos/restaurantfood/infrastructure/service/storage/LocalFotoStorageService.java
+++ restaurantfood-api/src/main/java/com/irmamsantos/restaurantfood/infrastructure/service/storage/LocalFotoStorageService.java
@@ -1,5 +1,6 @@
 package com.irmamsantos.restaurantfood.infrastructure.service.storage;
 
+import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
@@ -33,6 +34,17 @@ public class LocalFotoStorageService implements FotoStorageService {
 		} catch (Exception e) {
 			new StorageException("Não foi possível remover arquivo.", e);
 		}
+	}	
+	
+	@Override
+	public InputStream recuperar(String nomeArquivo) {
+		try {
+	        Path arquivoPath = getArquivoPath(nomeArquivo);
+
+	        return Files.newInputStream(arquivoPath);
+	    } catch (Exception e) {
+	        throw new StorageException("Não foi possível recuperar arquivo.", e);
+	    }
 	}	
 	
 	private Path getArquivoPath(String nomeArquivo) {
