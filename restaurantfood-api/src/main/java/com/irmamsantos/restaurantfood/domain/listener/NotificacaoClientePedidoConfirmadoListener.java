5a2ae75de0ffe2b68016ddbf8d0928d29dcc41e0
Irma Santos <irma.santos@gmail.com>
2020-05-17 23:23:07 +0100

    15.13 Reagindo a Domain Events em fases específicas da transação

--- restaurantfood-api/src/main/java/com/irmamsantos/restaurantfood/domain/listener/NotificacaoClientePedidoConfirmadoListener.java
+++ restaurantfood-api/src/main/java/com/irmamsantos/restaurantfood/domain/listener/NotificacaoClientePedidoConfirmadoListener.java
@@ -1,8 +1,9 @@
 package com.irmamsantos.restaurantfood.domain.listener;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.event.TransactionPhase;
+import org.springframework.transaction.event.TransactionalEventListener;
 
 import com.irmamsantos.restaurantfood.domain.event.PedidoConfirmadoEvent;
 import com.irmamsantos.restaurantfood.domain.model.Pedido;
@@ -15,7 +16,16 @@ public class NotificacaoClientePedidoConfirmadoListener {
 	@Autowired
 	private EnvioEmailService envioEmail;
 
-	@EventListener
+	/*
+	 * Usando @EventListener os eventos são gerados antes do update. E se update der erro, o mail já foi 
+	 * enviado. Se usar @TransactionalEventListener os eventos são gerados depois do commit, por omissão 
+	 * é AFTER_COMMIT. Mas neste caso fez commit e pode dar um erro no envio de mail. Cabe decidir se o 
+	 * mail é mesmo importante dentro da confirmação. Caso seja mesmo importante ao ponto de fazer rollback 
+	 * da confirmação caso não envie mail então usa-se BEFORE_COMMIT.
+	 */
+	
+	//@EventListener
+	@TransactionalEventListener(phase = TransactionPhase.BEFORE_COMMIT)
 	public void aoConfirmarPedido(PedidoConfirmadoEvent event) {
 		
 		Pedido pedido = event.getPedido();
