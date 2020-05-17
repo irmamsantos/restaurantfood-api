1913f299cb2b0ae1cc0df5c7edd67e0f08830e22
Irma Santos <irma.santos@gmail.com>
2020-05-17 22:24:16 +0100

    15.11 Publicando Domain Events a partir do Aggregate Root

--- restaurantfood-api/src/main/java/com/irmamsantos/restaurantfood/domain/model/Pedido.java
+++ restaurantfood-api/src/main/java/com/irmamsantos/restaurantfood/domain/model/Pedido.java
@@ -22,16 +22,18 @@ import javax.persistence.OneToMany;
 import javax.persistence.PrePersist;
 
 import org.hibernate.annotations.CreationTimestamp;
+import org.springframework.data.domain.AbstractAggregateRoot;
 
+import com.irmamsantos.restaurantfood.domain.event.PedidoConfirmadoEvent;
 import com.irmamsantos.restaurantfood.domain.exception.NegocioException;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 @Data
-@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@EqualsAndHashCode(onlyExplicitlyIncluded = true, callSuper = false)
 @Entity
-public class Pedido {
+public class Pedido extends AbstractAggregateRoot<Pedido> {
 	
 	@EqualsAndHashCode.Include
 	@Id
@@ -108,6 +110,8 @@ public class Pedido {
 	public void confirmar() {
 		setStatus(StatusPedido.CONFIRMADO);
 		setDataConfirmacao(OffsetDateTime.now());
+		
+		registerEvent(new PedidoConfirmadoEvent(this));
 	}
 	
 	public void entregar() {
--- restaurantfood-api/src/main/java/com/irmamsantos/restaurantfood/domain/service/FluxoPedidoService.java
+++ restaurantfood-api/src/main/java/com/irmamsantos/restaurantfood/domain/service/FluxoPedidoService.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.irmamsantos.restaurantfood.domain.model.Pedido;
-import com.irmamsantos.restaurantfood.domain.service.EnvioEmailService.Mensagem;
+import com.irmamsantos.restaurantfood.domain.repository.PedidoRepository;
 
 @Service
 public class FluxoPedidoService {
@@ -14,14 +14,18 @@ public class FluxoPedidoService {
 	@Autowired
 	private EmissaoPedidoService emissaoPedido;
 	
+/*	
 	@Autowired
 	private EnvioEmailService envioEmail;
-
+*/
+	@Autowired
+	private PedidoRepository pedidoRepository;
+	
 	@Transactional
 	public void confirmar(String codigoPedido) {
 		Pedido pedido = emissaoPedido.buscarOuFalhar(codigoPedido);
 		pedido.confirmar();
-		
+/*		
 		Mensagem mensagem = Mensagem.builder()
 				.assunto(pedido.getRestaurante().getNome() + " - Pedido Confirmado")
 //				.corpo("O pedido de código <strong>"
@@ -32,6 +36,13 @@ public class FluxoPedidoService {
 				.build();
 		
 		envioEmail.enviar(mensagem);
+*/		
+		/*
+		 * Não seria necessário o save para gravar o pedido porque fez get e está ser gerido pelo
+		 * EntityManager. A necessidade do save é para disparar o evento que está registado em 
+		 * Pedido. É como está feito a implementação do spring face ao disparo do evento.
+		 */
+		pedidoRepository.save(pedido);
 	}
 	
 	@Transactional
