cf8d00915f7644bf5f64a7dff2412f79c8cd5c91
Irma Santos <irma.santos@gmail.com>
2020-05-23 23:56:42 +0100

    16.7 Desafio: implementando uma requisição GET com JavaScript

--- algafood-js-client/client.js
+++ algafood-js-client/client.js
@@ -1,23 +1,28 @@
-function consultarRestaurantes() {
-    $.ajax({
-      url: "http://localhost:8080/restaurantes",
-      type: "get",
-  
-      success: function(response) {
-        $("#conteudo").text(JSON.stringify(response));
-      }
-    });
-  }
+function consultar() {
+  $.ajax({
+    url: "http://localhost:8080/formaPagamentos",
+    type: "get",
 
-  function fecharRestaurante() {
-    $.ajax({
-      url: "http://localhost:8080/restaurantes/1/fechamento",
-      type: "put",
-  
-      success: function(response) {
-        alert('Restaurante foi fechado');
-      }
-    });
-  }
-  
-  $("#botao").click(fecharRestaurante);
+    success: function(response) {
+      preencherTabela(response);
+    }
+  });
+}
+
+function preencherTabela(formasPagamento) {
+  $("#tabela tbody tr").remove();
+
+  $.each(formasPagamento, function(i, formaPagamento) {
+    var linha = $("<tr>");
+
+    linha.append(
+      $("<td>").text(formaPagamento.id),
+      $("<td>").text(formaPagamento.descricao)
+    );
+
+    linha.appendTo("#tabela");
+  });
+}
+
+
+$("#btn-consultar").click(consultar);
