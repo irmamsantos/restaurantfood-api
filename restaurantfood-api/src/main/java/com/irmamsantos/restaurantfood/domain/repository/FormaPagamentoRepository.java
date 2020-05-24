bc6093f6559ec1017615e17ea36dfd80a5d53ee0
Irma Santos <irma.santos@gmail.com>
2020-05-24 16:30:23 +0100

    17.8 Entendendo e preparando a implementação de Deep ETags

--- restaurantfood-api/src/main/java/com/irmamsantos/restaurantfood/domain/repository/FormaPagamentoRepository.java
+++ restaurantfood-api/src/main/java/com/irmamsantos/restaurantfood/domain/repository/FormaPagamentoRepository.java
@@ -1,6 +1,9 @@
 package com.irmamsantos.restaurantfood.domain.repository;
 
+import java.time.OffsetDateTime;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.irmamsantos.restaurantfood.domain.model.FormaPagamento;
@@ -8,4 +11,6 @@ import com.irmamsantos.restaurantfood.domain.model.FormaPagamento;
 @Repository
 public interface FormaPagamentoRepository extends JpaRepository<FormaPagamento, Long>{
 	
+	@Query("select max(dataAtualizacao) from FormaPagamento")
+	OffsetDateTime getDataUltimaAtualizacao();
 }
